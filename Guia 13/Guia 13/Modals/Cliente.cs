c8095951736c998869b0979580fbea35840e2edc
Guido Agustín González Battaglia <guido.agustinb@gmail.com>
2024-11-03 21:54:42 -0300

    Add files via upload

---
+++ Guia 13/Guia 13/Modals/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_13
+{
+    [Serializable]
+    internal class Cliente : Ticket
+    {
+        public long DNI { get;private set; }
+        private static int numero=1;
+
+        public Cliente(long d)
+        {
+            nroOrden = numero;
+            numero++;
+            DNI = d;
+        }
+
+
+        public override string ToString()
+        {
+            long d = DNI;
+            return base.ToString() + $"Cliente {d}";
+        }
+    }
+}
---
+++ Guia 13/Guia 13/Modals/Ticket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_13
+{
+    [Serializable]
+    abstract class Ticket : IComparable<Ticket>
+    {
+        public int nroOrden { get; protected set; }
+        private DateTime fechaHora = DateTime.Now;
+
+        public int VerNro()
+        {
+            return nroOrden;
+        }
+
+        public DateTime VerFechaHora()
+        {
+            return fechaHora;
+        }
+
+        public override string ToString()
+        {
+            int n = VerNro(); //DateTime f = VerFechaHora();
+            return $"Turno {n} - ";
+        }
+
+        //Opcional, no se para que usarlo pero se ve lindo
+        public int CompareTo(Ticket obj)
+        {
+            if (obj != null)
+            { return nroOrden.CompareTo(obj.nroOrden); }
+            return 1;
+        }
+
+    }
+}
